62a275702ca4ad35c0ee149cff8a4e1487f0b4ac
渡辺千華 <watanabechika@Einstein.local>
2019-03-25 22:07:29 +0900

    文法エラー修正

--- main2.js
+++ main2.js
@@ -6,6 +6,7 @@ var todoStorage = {
       todo.id = index
     })
     todoStorage.uid = todos.length
+    console.log(todos);
     return todos
   },
   save: function(todos) {
@@ -34,7 +35,7 @@ var filters = {
 // var Vue instance
 var app = new Vue({
   data: {
-    todos: todoStorage.fetch(), // fetch の使い方
+    todos: [],//todoStorage.fetch(), // fetch の使い方
     newTodo: '',
     editedTodo: null,
     visibility: 'all'
@@ -51,7 +52,7 @@ var app = new Vue({
   },
   // computed properties
   computed: {
-    filterdTodos: function() {
+    filteredTodos: function() {
       return filters[this.visibility](this.todos)
     },
     remaining: function() {
@@ -65,63 +66,63 @@ var app = new Vue({
         this.todos.forEach(function(todo){
           todo.completed = value
         })
+      }
     }
-  }
-},
-filters: {
-  pluralize: function(n) {
-    return n === 1 ? 'item' : 'items'
-  }
-},
-
-methods: {
-  addTodo: function() {
-    var value = this.newTodo && this.newTodo.trim()
-    if (!value) {
-      return
+  },
+  filters: {
+    pluralize: function(n) {
+      return n === 1 ? 'item' : 'items'
     }
-    this.todos.push({
-      id: todoStorage.uid++, // TODO
-      title: value,
-      completed: false
-    })
-    this.newTodo = ''
   },
 
-  removeTodo: function(todo) {
-    this.todos.splice(this.todos.indexof(todo), 1)
-  },
-  editTodo: function(todo) {
-    this.beforeEditCache = todo.title
-    this.editedTodo = todo
-  },
-  doneEdit: function(todo) {
-    if (!this.editedTodo) {
-      return
-    }
-    this.editedTodo = null
-    todo.title = todo.title.trim()
-    if (!todo.title) {
-      this.removeTodo(todo)
-    }
-  },
-  cancelEdit: function(todo) {
-    this.editedTodo = null
-    todo.title = this.beforeEditCache
-  },
+  methods: {
+    addTodo: function() {
+      var value = this.newTodo && this.newTodo.trim()
+      if (!value) {
+        return
+      }
+      this.todos.push({
+        id: todoStorage.uid++, // TODO
+        title: value,
+        completed: false
+      })
+      this.newTodo = ''
+    },
 
-  removeCompleted: function() {
-    this.todos = filters.active(this.todos)
-  }
-},
-// 内部と構成　カスタムディレクティブ参照
-directives: {
-  'todo-focus': function(el, binding) {
-    if (binding.value) {
-      el.focus()
+    removeTodo: function(todo) {
+      this.todos.splice(this.todos.indexof(todo), 1)
+    },
+    editTodo: function(todo) {
+      this.beforeEditCache = todo.title
+      this.editedTodo = todo
+    },
+    doneEdit: function(todo) {
+      if (!this.editedTodo) {
+        return
+      }
+      this.editedTodo = null
+      todo.title = todo.title.trim()
+      if (!todo.title) {
+        this.removeTodo(todo)
+      }
+    },
+    cancelEdit: function(todo) {
+      this.editedTodo = null
+      todo.title = this.beforeEditCache
+    },
+
+    removeCompleted: function() {
+      this.todos = filters.active(this.todos)
+    }
+  },
+  // 内部と構成　カスタムディレクティブ参照
+  directives: {
+    'todo-focus': function(el, binding) {
+      if (binding.value) {
+        el.focus()
+      }
     }
   }
-}
 })
 
 // handle routing
@@ -129,10 +130,10 @@ function onHashChange() {
   var visibility = window.location.hash.replace(/#\/?/, '')
   if (filters[visibility]){
     app.visibility = visibility
-} else {
-  window.location.hash = ''
-  app.visibility = 'all'
-}
+  } else {
+    window.location.hash = ''
+    app.visibility = 'all'
+  }
 }
 
 window.addEventListener('hashchange', onHashChange)
